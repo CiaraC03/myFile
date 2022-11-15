7adf56429bcc901f1e55b4a1babe6f2930e5b448
G00402357 <G00402357@atu.ie>
2022-11-15 15:17:16 +0000

    Added new info

--- src/main/java/ie/atu/week8.java
+++ src/main/java/ie/atu/week8.java
@@ -1,27 +1,30 @@
 package ie.atu;
 
+import java.util.Scanner;
 import java.io.*;
 import java.nio.file.Path;
 
 public class week8 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        try
-                {
-                    PrintWriter myWriter = new PrintWriter(new FileWriter("names.txt", true));
-                    myWriter.write("hello");
-                    myWriter.close();
-                } catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the filename: ");
+        String filename = keyboard.nextLine();
 
 
+        try {
+            Scanner inputFile = new Scanner(new File(filename));
+            while(inputFile.hasNext()) {
+                String str = inputFile.nextLine();
+                System.out.println(str);
+            }
+            inputFile.close();
 
-
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
+    }
     }
 
